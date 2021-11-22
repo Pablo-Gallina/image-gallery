9ac8d6e34ffbe113ade9c99a2e3dfcf677b35e8c
Pablo Gallina <pablogallinag@gmail.com>
2021-11-21 20:12:21 -0600

    Buscar una imagen agregado

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -3,26 +3,54 @@ import Card from './Card'
 import './Cards.css'
 
 const Cards = () => {
+    const [images, setImages] = useState([]);
+    //Search
+    const [inputSearch, setInputSearch] = useState("");
+
     const services = {
         url:"https://api.unsplash.com/",
-        path:"/photos/",
+        urlPhotos:"/photos/",
+        urlSearch: "/search/photos",
         urlKey:"?client_id=",
+        urlKeySearch:`?query=${inputSearch}`,
         key: "dYUO0r_XcG3_bkcB5FRgXsiLQGjE5nGvijrRml1SLGs"
     }
-    const [images, setImages] = useState([]);
 
-    const loadImage = async () => {
-        const res = await fetch(`${services.url}${services.path}${services.urlKey}${services.key}`);
+    const loadImage = async ( _url = "" ) => {
+        const RandomPhotos = `${services.url}${services.urlPhotos}${services.urlKey}${services.key}`;
+        const searchPhoto = `${services.url}${services.urlSearch}${services.urlKeySearch}&client_id=${services.key}`;
+
+        console.log(RandomPhotos);
+        console.log(searchPhoto);
+
+        const peticionUrl = _url !== "" ? searchPhoto : RandomPhotos;
+        
+        const res = await fetch(peticionUrl);
         const data = await res.json();
         console.log(data);
-        setImages(data)
+
+        if (data.results)
+            setImages(data.results)
+        else
+            setImages(data)
     }
     useEffect(() => {  
-        loadImage();
-    }, []);
+        loadImage(inputSearch);
+    }, [inputSearch]);
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        const valueInput = e.target[0].value;
+        setInputSearch(valueInput);
+    }
+
 
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <label>Buscar Imagen <input type="text" /> </label>
+            </form>
+            <br />
             <div className="container-flex">
                 {
                     images.map(img => {
